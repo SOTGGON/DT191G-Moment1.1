e3882930aee8cdb12ead0aa7ace9e465d7d0d9b1
SOTGGON <maoxiehui@outlook.com>
2024-01-24 18:57:56 +0100

    css och undersidor

--- Razor/wwwroot/js/site.js
+++ Razor/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Få aktuellt datum
+    var currentDate = new Date();
+
+    // Formatera datumet som xxxx-xx-xx
+    var formattedDate = currentDate.toISOString().slice(0, 10);
+
+    // Infoga datum på sidan
+    var dateElement = document.querySelector('.date');
+    if (dateElement) {
+        dateElement.textContent = 'Dagens datum är: ' + formattedDate;
+    }
+});
